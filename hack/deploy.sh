dbb81e5dd48ac8d5074523e7c890463f83fc645d
Pengfei Ni <feiskyer@gmail.com>
2017-01-23 23:06:11 +0800

    Update travis

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -e
 
+# generate sites
 hexo clean
 hexo generate
+
+# config git
+git config --global user.name "Pengfei Ni"
+git config --global user.email feiskyer@gmail.com
+git clone -b master https://${GitHubKEY}@github.com/feiskyer/feiskyer.github.io.git .deploy_git
+
+# push new sites
 cd .deploy_git
 rm -rf ./*
 cp -r ../public/* .
