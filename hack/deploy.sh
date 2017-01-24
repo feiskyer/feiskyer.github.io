84a50e2153cd4544f0329ada7edc27946689947d
Pengfei Ni <feiskyer@gmail.com>
2017-01-24 10:31:19 +0800

    Update deploy.sh

--- hack/deploy.sh
+++ hack/deploy.sh
@@ -10,10 +10,25 @@ git config --global user.name "Pengfei Ni"
 git config --global user.email feiskyer@gmail.com
 git clone -b master https://${GitHubKEY}@github.com/feiskyer/feiskyer.github.io.git .deploy_git
 
-# push new sites
+# update new sites
 cd .deploy_git
 rm -rf ./*
 cp -r ../public/* .
+cat >Gemfile <<EOF
+source 'https://rubygems.org'
+
+gem 'github-pages'
+EOF
+cat >.travis.yml <<EOF
+language: ruby
+
+rvm:
+- 2.1
+
+script: "bundle exec jekyll build"
+EOF
+
+# push new sites
 git add -A .
 git commit -a -m 'Site updated'
 git push -q -u origin master
