645739696b134da8e13ffb53291d69a5374397e8
Pengfei Ni <feiskyer@gmail.com>
2018-10-15 00:08:10 +0800

    Fix hugo installation

--- hack/hugo.sh
+++ hack/hugo.sh
@@ -3,5 +3,5 @@
 VERSION="0.49.2"
 wget https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz
 tar zxvf hugo_${VERSION}_Linux-64bit.tar.gz
-mv hugo /usr/local/bin
+sudo mv hugo /usr/local/bin
 
