c1bf8972f1e5e0f014ad7c7412773ee1692cbaa2
Pengfei Ni <feiskyer@gmail.com>
2020-02-02 19:31:02 +0800

    Fix

--- hack/hugo.sh
+++ hack/hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.49.2"
+VERSION="0.62.2"
 wget https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz
 tar zxvf hugo_${VERSION}_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin
