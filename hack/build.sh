99d218f6b7b5b2842e43d2296ffc775a482a57c5
Pengfei Ni <feiskyer@gmail.com>
2021-01-21 18:34:22 +0800

    Update site

--- hack/build.sh
+++ hack/build.sh
@@ -9,15 +9,21 @@ git clone -b master https://${GitHubKEY}@github.com/feiskyer/feiskyer.github.io.
 # update new sites
 rm -rf .deploy_git/*
 hugo -b https://feisky.xyz/ -D -E -d .deploy_git
-cp README.md .deploy_git
+
+cd .deploy_git
+cat >README.md <<EOF
+# 个人博客
+
+<https://feisky.xyz>
+EOF
 
 # Update travis
-cd .deploy_git
 cat >Gemfile <<EOF
 source 'https://rubygems.org'
 
 gem 'github-pages'
 EOF
+
 cat >.travis.yml <<EOF
 language: ruby
 
