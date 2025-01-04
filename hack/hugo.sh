f82c4a700f579d9e78a656f324e861c08833df7f
Pengfei Ni <feiskyer@gmail.com>
2025-01-04 15:24:51 +0800

    commit

--- hack/hugo.sh
+++ hack/hugo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION=$(curl -sSI https://github.com/gohugoio/hugo/releases/latest | awk -F "/" '/location/{print $NF}' | tr -d 'v')
+VERSION=$(curl --silent "https://api.github.com/repos/gohugoio/hugo/releases/latest" | jq -r .tag_name | tr -d 'v')
 wget https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_${VERSION}_Linux-64bit.tar.gz
 tar zxvf hugo_${VERSION}_Linux-64bit.tar.gz
 sudo mv hugo /usr/local/bin
